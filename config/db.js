a6656b18ea14355d9ce7f8584a77c23be089e968
David Urbina <durbina1991@gmail.com>
2016-04-27 16:46:59 -0700

    Jaws DB

--- config/db.js
+++ config/db.js
@@ -2,12 +2,17 @@
 
 var mysql = require("mysql");
 
-var database = mysql.createConnection({
+var database = mysql.createConnection(process.env.JAWSDB_URL || {
     host : "localhost",
     user : "root",
     "password" : "Ibanez19",
     "database" : "houseInventory"
 });
+
+
+database.query(`CREATE TABLE IF NOT EXISTS listOfRooms VALUES (roomNumber INTEGER, room TEXT)`);
+
+
 
 database.connect();
 
