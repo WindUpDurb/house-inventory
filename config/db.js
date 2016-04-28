c71e024daad2f765605567d5bf0104a72f9ec5af
David Urbina <durbina1991@gmail.com>
2016-04-27 17:11:14 -0700

    JAWS DB revisions

--- config/db.js
+++ config/db.js
@@ -2,18 +2,15 @@
 
 var mysql = require("mysql");
 
-var database = mysql.createConnection(process.env.JAWSDB_URL || {
+var database = mysql.createConnection("mysql://d2d0kcj5hc7r1ohz:cb2gsgzi205houee@tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bdydhhppkz47fixx" || {
     host : "localhost",
     user : "root",
     "password" : "Ibanez19",
     "database" : "houseInventory"
 });
 
-
-database.query(`CREATE TABLE IF NOT EXISTS listOfRooms VALUES (roomNumber INTEGER, room TEXT)`);
-
-
-
 database.connect();
+
+database.query(`CREATE TABLE IF NOT EXISTS listOfRooms (roomNumber INT, room TEXT)`);
 
 module.exports = database;
